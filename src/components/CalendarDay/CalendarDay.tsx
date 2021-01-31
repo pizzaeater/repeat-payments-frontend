6e1381b4694b6c5b1228efae121934075a926505
Olga Gidion <olga.gidion@gmail.com>
2021-01-31 15:57:00 +0100

    Using bem-react-classname package

--- src/components/CalendarDay/CalendarDay.tsx
+++ src/components/CalendarDay/CalendarDay.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
+import { createCn } from 'bem-react-classname';
 import './CalendarDay.scss';
+
+const cn = createCn('CalendarDay');
 
 interface Props {
   date: Date
   type: 'today' | 'income' | 'expense' | 'inactive'
 }
 
-const CalendarDay: React.FC<Props> = ({ date, type }) => {
-  const dayClassSet = type === 'inactive'
-    ? 'CalendarDay__day CalendarDay__day_inactive'
-    : 'CalendarDay__day';
-
-  const monthMod = `CalendarDay__month_type_${type}`;
-
-  return (
-    <div className="CalendarDay">
-      <div className={`CalendarDay__month ${monthMod}`}>{date.toLocaleString('default', { month: 'short'}).toLocaleUpperCase()}</div>
-      <div className={dayClassSet}>{date.getDate()}</div>
+const CalendarDay: React.FC<Props> = ({ date, type }) => (
+  <div className={cn()}>
+    <div className={cn('month', { type })}>
+      {date.toLocaleString('default', { month: 'short' }).toLocaleUpperCase()}
     </div>
-  )
-};
+    <div className={cn('day', { inactive: type === 'inactive' })}>
+      {date.getDate()}
+    </div>
+  </div>
+);
 
 export default CalendarDay;
