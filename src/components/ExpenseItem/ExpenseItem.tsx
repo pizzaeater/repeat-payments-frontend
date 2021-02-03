b269b8d9f323b2aeb9b58fbbc72d6327ad50d007
Olga Gidion <olga.gidion@gmail.com>
2021-02-03 23:07:08 +0100

    Showing expense receiver

--- src/components/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -16,7 +16,10 @@ const ExpenseItem: React.FC<Props> = ({ expense }) => (
   <Panel inactive={expense.inactive}>
     <div className={cn({ inactive: expense.inactive })}>
       <CalendarDay date={expense.day.date} type="expense" inactive={expense.inactive} />
-      <div className={cn('text')}>{expense.name}</div>
+      <div className={cn('text')}>
+        <p className={cn('name')}>{expense.name}</p>
+        <p className={cn('receiver')}>{expense.receiver}</p>
+      </div>
       <Amount value={expense.price} />
     </div>
   </Panel>
