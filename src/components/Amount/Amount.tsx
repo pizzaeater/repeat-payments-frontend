2369f0cb9ab4bd2172b20d6b18ed601471ddfc13
Olga Gidion <olga.gidion@gmail.com>
2021-02-03 21:58:36 +0100

    Created Amount component

---
+++ src/components/Amount/Amount.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createCn } from 'bem-react-classname';
+import './Amount.scss';
+
+const cn = createCn('Amount');
+
+interface Props {
+  value: number
+}
+
+const Amount: React.FC<Props> = ({ value }) => {
+  const [ceils, cents] = value.toFixed(2).split('.');
+
+  return (
+    <div className={cn()}>
+      <div>{ceils}</div>
+      <div className={cn('cents')}>{cents}</div>
+      <div className={cn('currency')}>€</div>
+    </div>
+  );
+};
+
+export default Amount;
---
+++ src/components/Amount/index.ts
@@ -0,0 +1,3 @@
+import Component from './Amount';
+
+export default Component;
