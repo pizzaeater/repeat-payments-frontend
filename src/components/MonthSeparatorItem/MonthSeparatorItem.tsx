7a56b4a20d09ff993661a1eb969fa2cd378bd795
Olga Gidion <olga.gidion@gmail.com>
2021-02-05 14:48:28 +0100

    Created adaptiveness

--- src/components/MonthSeparatorItem/MonthSeparatorItem.tsx
+++ src/components/MonthSeparatorItem/MonthSeparatorItem.tsx
@@ -9,10 +9,15 @@ interface Props {
   monthSeparator: MonthSeparator
 }
 
-const MonthSeparatorItem: React.FC<Props> = ({ monthSeparator }) => (
-  <div className={cn()}>
-    {monthSeparator.monthStartDay.date.toLocaleString('default', { month: 'long', year: 'numeric' })}
-  </div>
-);
+const MonthSeparatorItem: React.FC<Props> = ({ monthSeparator }) => {
+  const date = monthSeparator.monthStartDay.date;
+
+  return (
+    <div className={cn()}>
+      <span className={cn('month')}>{date.toLocaleString('default', { month: 'long' })}</span>
+      <span className={cn('year')}>&nbsp;{date.toLocaleString('default', { year: 'numeric' })}</span>
+    </div>
+  );
+};
 
 export default MonthSeparatorItem;
